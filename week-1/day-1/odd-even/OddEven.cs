8c6629b5f60a16e574e0b7cb06ca43544c864ed6
Azam Khan <azamkhandev@gmail.com>
2021-03-24 21:41:49 +0000

    odd even done

--- week-1/day-1/odd-even/OddEven.cs
+++ week-1/day-1/odd-even/OddEven.cs
@@ -9,6 +9,18 @@ namespace PracticeTask
             // Write a program that reads a number from the standard input,
             // then prints "Odd" if the number is odd, or "Even" if it is even.
 
+            Console.WriteLine("Enter number to check if its even or odd");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
         }
     }
 }
