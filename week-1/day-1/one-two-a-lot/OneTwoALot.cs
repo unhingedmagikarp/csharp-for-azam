a43be599a2588ca90a064a60ab79c9ac6d974c7e
Azam Khan <azamkhandev@gmail.com>
2021-03-24 21:48:17 +0000

    onetwoalot done

--- week-1/day-1/one-two-a-lot/OneTwoALot.cs
+++ week-1/day-1/one-two-a-lot/OneTwoALot.cs
@@ -11,7 +11,25 @@ namespace PracticeTask
             // If the number is one it should print: One
             // If the number is two it should print: Two
             // If the number is more than two it should print: A lot
+            Console.WriteLine("Enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
 
+            if (number <= 0)
+            {
+                Console.WriteLine("Not enough");
+            }
+            else if (number == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else
+            {
+                Console.WriteLine("A lot");
+            }
         }
     }
 }
