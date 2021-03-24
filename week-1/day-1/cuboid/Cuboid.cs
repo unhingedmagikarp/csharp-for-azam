212de8d76b07a3d023b755cdb14c60677e25437b
Azam Khan <azamkhandev@gmail.com>
2021-03-24 16:48:48 +0000

    cuboid done

--- week-1/day-1/cuboid/Cuboid.cs
+++ week-1/day-1/cuboid/Cuboid.cs
@@ -11,6 +11,21 @@ namespace PracticeTask
             //
             // Surface Area: 600
             // Volume: 1000
+            Console.WriteLine("Calculate surface area and volume of a cuboid");
+            Console.WriteLine("Enter Length: ");
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Height: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Width: ");
+            double w = Convert.ToDouble(Console.ReadLine());
+
+
+            double volumeCuboid = (l * h * w);
+
+            double surfaceAreaCuboid = (2 * l * w + 2 * w * h + 2 * l * h);
+
+            Console.WriteLine("Surface Area: " + surfaceAreaCuboid);
+            Console.WriteLine("Volume: " + volumeCuboid);
         }
     }
 }
