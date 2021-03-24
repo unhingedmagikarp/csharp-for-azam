3d7219f0507602d1a8ebcbdeb66647a25b0d20a6
Azam Khan <azamkhandev@gmail.com>
2021-03-24 21:52:44 +0000

    print bigger

--- week-1/day-1/print-bigger/PrintBigger.cs
+++ week-1/day-1/print-bigger/PrintBigger.cs
@@ -7,6 +7,13 @@ namespace PracticeTask
         static void Main(string[] args)
         {
             // Write a program that asks for two numbers and prints the bigger one
+            Console.WriteLine("Enter two numbers to check which is better");
+            Console.WriteLine("Enter first number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int highest = Math.Max(number1, number2);
+            Console.WriteLine(highest + " is the biggest");
         }
     }
 }
