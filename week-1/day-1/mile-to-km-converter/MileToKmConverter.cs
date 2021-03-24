0ba898fa039ced32ac8cc9bddd314acbb3c620dc
Azam Khan <azamkhandev@gmail.com>
2021-03-24 17:15:51 +0000

    mile to km done

--- week-1/day-1/mile-to-km-converter/MileToKmConverter.cs
+++ week-1/day-1/mile-to-km-converter/MileToKmConverter.cs
@@ -8,6 +8,11 @@ namespace PracticeTask
         {
             // Write a program that asks for a double that is a distance in miles,
             // then it converts that value to kilometers and prints it
+
+            Console.WriteLine("Enter your distance in miles");
+            double distanceInMiles = Convert.ToDouble(Console.ReadLine());
+            double calcDistanceInKm = distanceInMiles * 1.609;
+            Console.WriteLine("Distance in Kilometers is " + calcDistanceInKm);
         }
     }
 }
