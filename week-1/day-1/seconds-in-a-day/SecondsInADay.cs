9104fa5dc2866282f99675a5695b23b72ab9aae2
Azam Khan <azamkhandev@gmail.com>
2021-03-24 17:08:45 +0000

    secondsinday done

--- week-1/day-1/seconds-in-a-day/SecondsInADay.cs
+++ week-1/day-1/seconds-in-a-day/SecondsInADay.cs
@@ -12,6 +12,14 @@ namespace PracticeTask
 
             // Write a program that prints the remaining seconds (as an integer) from a
             // day if the current time is represented by the variables above
+            int totalSecondsInDay = 86400;
+
+            int remainingSecondsFromMinutes = currentMinutes * 60;
+            int remainingSecondsFromHours = currentHours * 3600;
+
+            int remainingSecondsFromDay = totalSecondsInDay - remainingSecondsFromHours - remainingSecondsFromMinutes - currentSeconds;
+
+            Console.WriteLine("Remaining Seconds: " + remainingSecondsFromDay);
         }
     }
 }
