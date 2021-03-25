8546947b1962b332ac5f9444bce1f3c7f9540c24
Azam Khan <azamkhandev@gmail.com>
2021-03-25 21:33:58 +0000

    printeven

--- week-1/day-1/print-even/PrintEven.cs
+++ week-1/day-1/print-even/PrintEven.cs
@@ -7,6 +7,14 @@ namespace PracticeTask
         static void Main(string[] args)
         {
             // Create a program that prints all the even numbers between 0 and 500
+            for (int i = 0; i < 500; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
