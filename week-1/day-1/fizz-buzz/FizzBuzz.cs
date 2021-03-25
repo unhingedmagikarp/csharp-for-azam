01e484439f2565eabbc0d1eaaa047a5066a990d9
Azam Khan <azamkhandev@gmail.com>
2021-03-25 22:04:38 +0000

    fizzbuzz done

--- week-1/day-1/fizz-buzz/FizzBuzz.cs
+++ week-1/day-1/fizz-buzz/FizzBuzz.cs
@@ -10,6 +10,26 @@ namespace PracticeTask
             // but for multiples of three print “Fizz” instead of the number
             // and for the multiples of five print “Buzz”
             // For numbers which are multiples of both three and five print “FizzBuzz”
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 & i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
