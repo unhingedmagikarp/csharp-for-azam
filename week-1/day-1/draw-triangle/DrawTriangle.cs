ef90951392e7f40dbf7bd4d989cda4a287243446
Azam Khan <azamkhandev@gmail.com>
2021-03-26 22:50:02 +0000

    draw traingle done

--- week-1/day-1/draw-triangle/DrawTriangle.cs
+++ week-1/day-1/draw-triangle/DrawTriangle.cs
@@ -15,6 +15,17 @@ namespace PracticeTask
             // ****
             //
             // The triangle should have as many lines as the number was
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
