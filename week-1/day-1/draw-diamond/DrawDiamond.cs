892524bc417443c70a452fe181676685feffc31e
Azam Khan <azamkhandev@gmail.com>
2021-03-28 14:27:37 +0100

    draw diamond done

--- week-1/day-1/draw-diamond/DrawDiamond.cs
+++ week-1/day-1/draw-diamond/DrawDiamond.cs
@@ -19,7 +19,24 @@ namespace PracticeTask
             //    *
             //
             // The diamond should have as many lines as the number was
-
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= num; i++)
+            {
+                for (int j = 1; j <= num - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            for (int i = num - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= num - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
         }
     }
 }
