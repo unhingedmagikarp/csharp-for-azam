3ecaa61277ee857a6b0256ac3e7a16a8cc16d024
Azam Khan <azamkhandev@gmail.com>
2021-03-28 23:23:10 +0100

    doubling

--- week-1/day-2/functions/doubling/Doubling.cs
+++ week-1/day-2/functions/doubling/Doubling.cs
@@ -10,7 +10,12 @@ namespace Doubling
             // - Create a function called `DoubleNumber()` that doubles it's input parameter
             //   and returns the doubled value
             // - Print the result of `DoubleNumber(baseNumber)`
-            Console.ReadLine();
+            int baseNumber = 6;
+            DoubleNumber(baseNumber);
+        }
+        static void DoubleNumber(int input)
+        {
+            Console.WriteLine(input * 2);
         }
     }
 }
