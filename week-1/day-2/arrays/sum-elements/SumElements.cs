9e87ddb0177da2ba040792cd5ab2d29c3cbb3e00
Azam Khan <azamkhandev@gmail.com>
2021-03-28 21:26:29 +0100

    sum elements done

--- week-1/day-2/arrays/sum-elements/SumElements.cs
+++ week-1/day-2/arrays/sum-elements/SumElements.cs
@@ -9,7 +9,8 @@ namespace SumElements
             // - Create an array variable named `numbers`
             //   with the following content: `[54, 23, 66, 12]`
             // - Print the sum of the second and the third element
-            Console.ReadLine();
+            int[] numbers = { 54, 23, 66, 12 };
+            Console.WriteLine(numbers[1] + numbers[2]);
         }
     }
 }
