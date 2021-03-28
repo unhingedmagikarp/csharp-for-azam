3d0b11002b29c4cbd2aba775c33f9b5089c7fd43
Azam Khan <azamkhandev@gmail.com>
2021-03-29 00:11:17 +0100

    sum done

--- week-1/day-2/functions/sum/Sum.cs
+++ week-1/day-2/functions/sum/Sum.cs
@@ -7,6 +7,19 @@ namespace Sum
         static void Main(string[] args)
         {
             // Write a function called `sum()` that returns the sum of numbers from zero to the given parameter
+            int num = 5;
+            // 5 + 4 + 3 + 2 + 1 + 0 = 15
+            Sum(num);
+        }
+
+        static void Sum(int input)
+        {
+            int sum = 0;
+            for (int i = 0; i <= input; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
