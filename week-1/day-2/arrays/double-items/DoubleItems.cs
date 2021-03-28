77a2dc2bafcf2650416e6c5f0fa9a427bbf1f98b
Azam Khan <azamkhandev@gmail.com>
2021-03-28 22:16:19 +0100

    doubleitems done

--- week-1/day-2/arrays/double-items/DoubleItems.cs
+++ week-1/day-2/arrays/double-items/DoubleItems.cs
@@ -9,7 +9,11 @@ namespace DoubleItems
             // - Create an array variable named `numbers`
             //   with the following content: `[3, 4, 5, 6, 7]`
             // - Double all the values in the array
-            Console.ReadLine();
+            int[] numbers = { 3, 4, 5, 6, 7 };
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number * 2);
+            }
         }
     }
 }
