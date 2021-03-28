ccdf0d14c04c77b00b03bcc9aa7d95f3e9d19b3f
Azam Khan <azamkhandev@gmail.com>
2021-03-28 15:19:56 +0100

    draw diagonal done

--- week-1/day-1/draw-diagonal/DrawDiagonal.cs
+++ week-1/day-1/draw-diagonal/DrawDiagonal.cs
@@ -17,7 +17,26 @@ namespace PracticeTask
             // %%%%%
             //
             // The square should have as many lines as the number was
+            // n = rows and m = columns
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int columns = 5;
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= columns; j++)
+                {
+                    if (i == 1 || i == num ||
+                        j == 1 || j == columns)
+                        Console.Write("%");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
 
+
+            //something close to this, i cant make it work
+            for (x = 0; x < n; r += (y % n * x < 1) | (y == x) | (y == n - x++) ? "*" : " ") ;
         }
     }
 }
