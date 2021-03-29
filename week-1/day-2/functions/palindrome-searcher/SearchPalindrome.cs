9ccfb5c00cec511a7073e69972b16e1f5d0248f0
Azam Khan <azamkhandev@gmail.com>
2021-03-29 17:14:16 +0100

    SearchPalindrome done

---
+++ week-1/day-2/functions/palindrome-searcher/SearchPalindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PracticeTask
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            SearchPalindrome("dog goat dad duck doodle never");
+
+        }
+
+        private static void SearchPalindrome(string input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = i + 1; j <= input.Length; j++)
+                {
+                    if (j - i > 1 && input[j - 1] == input[i])
+                    {
+                        string currentSubset = input.Substring(i, j - i);
+                        Console.WriteLine(currentSubset);
+                    }
+                }
+            }
+        }
+    }
+}
