3128982e02eb46daa30cd6b5167eb82c0fdef81a
Azam Khan <azamkhandev@gmail.com>
2021-03-30 12:10:47 +0100

    shoppinglist done

---
+++ week-1/day-3/shopping-list/ShoppingList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<String> shoppingList = new List<string>();
+            shoppingList.Add("Eggs");
+            shoppingList.Add("milk");
+            shoppingList.Add("fish");
+            shoppingList.Add("apples");
+            shoppingList.Add("bread");
+            shoppingList.Add("chicken");
+            Console.WriteLine(shoppingList.Contains("milk"));
+            Console.WriteLine(shoppingList.Contains("bananas"));
+        }
+
+    }
+}
