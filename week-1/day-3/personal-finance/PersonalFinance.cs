6b3b993718f25ed4903a4e64a25675feed042602
Azam Khan <azamkhandev@gmail.com>
2021-03-30 01:21:28 +0100

    PersonalFinance done

---
+++ week-1/day-3/personal-finance/PersonalFinance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonalFinance
+{
+    class Program
+    {
+        static List<int> expenses = new();
+        public static void Main()
+        {
+            expenses.Add(500);
+            expenses.Add(1000);
+            expenses.Add(1250);
+            expenses.Add(175);
+            expenses.Add(800);
+            expenses.Add(120);
+            TotalSpent();
+            GreatestExpense();
+            CheapestSpending();
+            AverageSpendings();
+
+        }
+
+        private static void TotalSpent()
+        {
+            Console.WriteLine(expenses.Sum());
+        }
+        private static void GreatestExpense()
+        {
+            Console.WriteLine(expenses.Max());
+        }
+        private static void CheapestSpending()
+        {
+            Console.WriteLine(expenses.Min());
+        }
+        private static void AverageSpendings()
+        {
+            Console.WriteLine(expenses.Average());
+        }
+    }
+}
