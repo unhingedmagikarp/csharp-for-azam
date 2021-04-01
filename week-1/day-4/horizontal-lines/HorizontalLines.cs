0c5fd234e40f1fc7382087e5b8c1f1adf360b628
Azam Khan <azamkhandev@gmail.com>
2021-04-01 16:58:11 +0100

    HorizontalLines done

--- week-1/day-4/horizontal-lines/HorizontalLines.cs
+++ week-1/day-4/horizontal-lines/HorizontalLines.cs
@@ -14,7 +14,19 @@ namespace HorizontalLines
             // Parameters: the x and y coordinates of the line's starting point and the foxDraw
             // The function shall draw a 50 long horizontal line from that point
             // Draw at least 3 lines with that function using a loop
-
+            DrawLotsOfLines(0, 212, foxDraw, canvas);
+            DrawLotsOfLines(15, 110.1, foxDraw, canvas);
+            DrawLotsOfLines(45, 380, foxDraw, canvas);
+        }
+        private void DrawLotsOfLines(double x, double y, FoxDraw foxDraw, Canvas canvas)
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                var startPoint1 = new Point(x, y);
+                var endPoint1 = new Point(canvas.Width, y);
+                foxDraw.SetStrokeColor(Colors.Red);
+                foxDraw.DrawLine(startPoint1, endPoint1);
+            }
         }
     }
 }
