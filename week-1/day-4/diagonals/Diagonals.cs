d0af1317515aa10989f9d479c7b207c20a51744c
Azam Khan <azamkhandev@gmail.com>
2021-04-01 16:19:17 +0100

    diagonals done

--- week-1/day-4/diagonals/Diagonals.cs
+++ week-1/day-4/diagonals/Diagonals.cs
@@ -9,11 +9,21 @@ namespace Diagonals
         public MainWindow()
         {
             InitializeComponent();
+            UpperLeftShouldBeGreen();
             var foxDraw = new FoxDraw(canvas);
             // Create a function that draws a single line
             // Use this function to draw the canvas' diagonals
             // If the line starts from the upper-left corner it should be green, otherwise it should be red
             // Make decision about the color in the function
+            
+        }
+        private void UpperLeftShouldBeGreen()
+        {
+            var upperLeftPoint = new Point(0, 0);
+            var startPoint1 = new Point(0, 0);
+            var endPoint1 = new Point(canvas.Width, canvas.Height);
+            foxDraw.SetStrokeColor(startPoint1.Equals(upperLeftPoint) ? Colors.Green : Colors.Red);
+            foxDraw.DrawLine(startPoint1, endPoint1);
         }
     }
 }
