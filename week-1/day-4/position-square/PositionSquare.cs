848ac823d3059bd2c12fc71068af97556e0255b1
Azam Khan <azamkhandev@gmail.com>
2021-04-01 17:27:24 +0100

    positionsqauare done

--- week-1/day-4/position-square/PositionSquare.cs
+++ week-1/day-4/position-square/PositionSquare.cs
@@ -15,7 +15,17 @@ namespace PositionSquare
             // The function shall draw a 50x50 square from that point
             // Draw 3 squares with that function
             // Avoid code duplication!
-
+            Draw3Squares(0, 212, foxDraw, canvas);
+            Draw3Squares(15, 110.1, foxDraw, canvas);
+            Draw3Squares(45, 380, foxDraw, canvas);
+        }
+        private void Draw3Squares(double x, double y, FoxDraw foxDraw, Canvas canvas)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                foxDraw.SetStrokeColor(Colors.Red);
+                foxDraw.DrawRectangle(x, y / 2, 50, 50);
+            }
         }
     }
 }
