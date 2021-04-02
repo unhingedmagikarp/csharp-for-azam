b85c10091bd597fcdf4010a334d43171c1416f4a
Azam Khan <azamkhandev@gmail.com>
2021-04-02 13:26:36 +0100

    StarryNight done

--- week-1/day-4/starry-night/StarryNight.cs
+++ week-1/day-4/starry-night/StarryNight.cs
@@ -15,7 +15,23 @@ namespace StarryNight
             //  - The stars can be small squares
             //  - The stars should have random positions on the canvas
             //  - The stars should have random color (some shade of grey)
+            for (int i = 0; i < 999; i++)
+            {
 
+                var random = new Random();
+                var randomSize = random.Next(1, 4);
+                var randomLocationX = random.Next(1, 800);
+                var randomLocationY = random.Next(1, 400);
+
+                string[] starColors = { "White", "AntiqueWhite", "Aqua", "Azure", "WhiteSmoke", "NavajoWhite", "FloralWhite" };
+
+                var randomColor = random.Next(0, starColors.Length);
+
+                foxDraw.SetStrokeColor(Color.Parse($"{starColors[randomColor]}"));
+                foxDraw.SetStrokeThicknes(0);
+                foxDraw.SetFillColor(Color.Parse($"{starColors[randomColor]}"));
+                foxDraw.DrawRectangle(randomLocationX, randomLocationY, randomSize, randomSize);
+            }
         }
     }
 }
