fcaac323462bca4a5a285426051aa1b23019927c
Azam Khan <azamkhandev@gmail.com>
2021-04-03 02:14:12 +0100

    ReversedLines done

--- week-1/day-5/decrypt-reversed-lines/ReversedLines.cs
+++ week-1/day-5/decrypt-reversed-lines/ReversedLines.cs
@@ -1,1 +1,31 @@
-// Create a method that decrypts reversed-lines.txt
+using System;
+using System.IO;
+using System.Linq;
+
+namespace ReversedLines
+{
+    class ReversedLines
+    {
+        public static void Main()
+        {
+            string fileLocation = @"S:\Work\Code\peter-keller\csharp-for-azam\week-1\day-5\assets\reversed-lines.txt";
+            ReversedLines(fileLocation);
+
+        }
+        private static void ReversedLines(string fileLocation)
+        {
+            StreamReader myReader = new StreamReader(fileLocation);
+            var line = myReader.ReadToEnd();
+            myReader.Close();
+
+            using (var writer = new StreamWriter(fileLocation))
+            {
+                char[] charArray = line.ToCharArray();
+                Array.Reverse(charArray);
+                var output = new string(charArray);
+                Console.WriteLine(output);
+                writer.Write(output);
+            }
+        }
+    }
+}
