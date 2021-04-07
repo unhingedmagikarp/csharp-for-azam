1ebf447ce7c80cc376e6d35d435b5fc62fb21fa2
Azam Khan <azamkhandev@gmail.com>
2021-04-07 21:13:04 +0100

    this was not centered, fixed this and made it into a function that makes sure its centered depending on its size

--- week-1/day-4/centered-square/CenteredSquare.cs
+++ week-1/day-4/centered-square/CenteredSquare.cs
@@ -1,5 +1,7 @@
-using System;
-using System.Windows;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using GreenFox;
 
 namespace CenteredSquare
@@ -11,7 +13,18 @@ namespace CenteredSquare
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
             // Draw a green 10x10 square to the canvas' center
-            foxDraw.DrawRectangle(canvas.Width / 2, canvas.Height / 2, 10, 10);
+            CenteredSquare(10, foxDraw, canvas);
+        }
+        private void CenteredSquare(double size, FoxDraw foxDraw, Canvas canvas)
+        {
+            // Center the square
+            double halfOfWidth = canvas.Width / 2;
+            double halfOfHeight = canvas.Height / 2;
+            double x = halfOfWidth - size / 2;
+            double y = halfOfHeight - size / 2;
+
+            foxDraw.SetFillColor(Colors.Green);
+            foxDraw.DrawRectangle(x, y, size, size);
         }
     }
 }
