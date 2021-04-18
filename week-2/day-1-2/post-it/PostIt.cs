394e6a39bf227a353b00acfbd13542befc53f3a5
Azam Khan <azamkhandev@gmail.com>
2021-04-18 19:18:59 +0100

    post it done

---
+++ week-2/day-1-2/post-it/PostIt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2.day12
+{
+    class PostIt
+    {
+        private string _backgroundColor;
+        private string _text;
+        private string _textColor;
+
+        public PostIt(string backgroundColor, string text, string textColor)
+        {
+            _backgroundColor = backgroundColor;
+            _text = text;
+            _textColor = textColor;
+        }
+    }
+}
---
+++ week-2/day-1-2/post-it/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace week2.day12
+{
+    class Program
+    {
+        public static void Main()
+        {
+            /*
+            - Create a `PostIt` a struct that has
+               - a `BackgroundColor`
+               - a `Text` on it
+               - a `TextColor`
+               - Create a few example post-it objects:
+               - an orange with blue text: "Idea 1"
+               - a pink with black text: "Awesome"
+               - a yellow with green text: "Superb!"
+
+             */
+
+            PostIt example1 = new PostIt("orange", "Idea 1", "blue");
+            PostIt example2 = new PostIt("pink", "Awesome", "black");
+            PostIt example3 = new PostIt("yellow", "Superb!", "green");
+
+        }
+    }
+}
