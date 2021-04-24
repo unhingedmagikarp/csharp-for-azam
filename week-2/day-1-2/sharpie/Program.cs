9ba12243cdc8c714fe01b13a20ae95c185df7a6e
Azam Khan <azamkhandev@gmail.com>
2021-04-24 18:20:47 +0100

    sharpie done

---
+++ week-2/day-1-2/sharpie/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace week2.day12
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            Sharpie s1 = new Sharpie("blue", 5);
+
+            s1.Use();
+
+            Console.WriteLine(s1._inkAmount);
+
+        }
+    }
+}
---
+++ week-2/day-1-2/sharpie/Sharpie.cs
@@ -0,0 +1,34 @@
+﻿namespace week2.day12
+{
+    /*
+- Create a `Sharpie` class
+       - We should know the followings about each sharpie:
+       - `color` (which should be a string),
+       - `width` (which will be a floating point number) and the
+       - `inkAmount` (another floating point number)
+       - We need to specify the `color` and the `width` at creation
+       - Every sharpie is created with a default `inkAmount` value: `100`
+       - We can `Use()` the sharpie objects: using it decreases
+       `inkAmount` by `10`
+     
+     */
+    class Sharpie
+    {
+        public string _color;
+        public float _width;
+        public float _inkAmount;
+
+        public Sharpie(string color, float width)
+        {
+            _color = color;
+            _width = width;
+            _inkAmount = 100;
+        }
+
+        public void Use()
+        {
+            _inkAmount -= 10;
+        }
+
+    }
+}
