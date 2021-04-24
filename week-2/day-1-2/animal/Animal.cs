5e8c1ee70c2814d3dce8e6d416f24d18404b2975
Azam Khan <azamkhandev@gmail.com>
2021-04-24 16:45:32 +0100

    Animals done

---
+++ week-2/day-1-2/animal/Animal.cs
@@ -0,0 +1,42 @@
+﻿namespace week2.day12
+{
+    class Animal
+    {
+        /*
+         - Create an `Animal` class
+           - Every animal has a `hunger` value, which is a whole number
+           - Every animal has a `thirst` value, which is a whole number
+           - When creating a new animal instance these values must be set to the
+           default `50` value
+           - Every animal can `eat()` which decreases its hunger by one
+           - Every animal can `drink()` which decreases its thirst by one
+           - Every animal can `play()` which increases both its hunger and thirst by one
+         */
+
+        public int Hunger = 50;
+        public int Thirst = 50;
+
+        public Animal(int hunger, int thirst)
+        {
+            Hunger = hunger;
+            Thirst = thirst;
+        }
+
+        public void Eat()
+        {
+            Hunger -= 1;
+        }
+
+        public void Drink()
+        {
+            Thirst -= 1;
+        }
+
+        public void Play()
+        {
+            Hunger += Hunger;
+            Thirst += Thirst;
+        }
+    }
+}
+    
---
+++ week-2/day-1-2/animal/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace InterviewPractice
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            Animal dog = new Animal(50, 50);
+
+            dog.Eat();
+
+            Console.WriteLine(dog.Hunger);
+
+        }
+    }
+}
