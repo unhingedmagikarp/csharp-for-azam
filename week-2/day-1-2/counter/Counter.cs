204fbaddd232f3671b9c43a33c6f920a5bc6c350
Azam Khan <azamkhandev@gmail.com>
2021-04-25 02:41:40 +0100

    counter done

---
+++ week-2/day-1-2/counter/Counter.cs
@@ -0,0 +1,43 @@
+﻿namespace week2.day12
+{
+    class Counter
+    {
+        /*
+         - Create `Counter` class
+           - which has a `counter` (integer) field
+           - at creation it should have a default value `0` or can be specified
+           by a parameter
+           - we can add another whole number to this counter: `add(number)`
+           - we can just increase the counter's value by one: `add()` (no parameters)
+           - we can `get()` the current counter value
+           - and we can `reset()` the counter to the initial value
+           
+         */
+        private int _counter = 0;
+
+        public Counter(int counter)
+        {
+            _counter = counter;
+        }
+
+        public void Add(int number)
+        {
+            _counter += number;
+        }
+        public void Add()
+        {
+            _counter += 1;
+        }
+
+        public int Get()
+        {
+            return _counter;
+        }
+
+        public int Reset()
+        {
+            return _counter = 0;
+        }
+
+    }
+}
---
+++ week-2/day-1-2/counter/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace week2.day12
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            Counter c1 = new Counter(10);
+
+            c1.Add(5);
+            c1.Add();
+            c1.Get();
+
+        }
+    }
+}
